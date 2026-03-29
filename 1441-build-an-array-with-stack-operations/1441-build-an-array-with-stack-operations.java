3e6e30eb8860afecdc8cd0160247d4e4d4113635
abhishek kumar yadav <ay997326@gmail.com>
2026-03-29 14:23:13 +0530

    Time: 0 ms (100%), Space: 44.2 MB (63.97%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        for (int num = 1; num <= n; num++) {
+            if (i >= target.length) break;
+            result.add("Push");
+            if (target[i] == num) {
+                i++;
+            } else {
+                result.add("Pop");
+            }
+        }
+        return result;
+    }
+}
