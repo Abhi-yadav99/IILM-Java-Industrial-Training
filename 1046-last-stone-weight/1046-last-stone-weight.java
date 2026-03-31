150ba34a616a68bbef26616bc6db3682fc9a5f00
abhishek kumar yadav <ay997326@gmail.com>
2026-03-31 23:36:48 +0530

    Time: 2 ms (36.26%), Space: 42.6 MB (95.45%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);        
+        for (int s : stones) {
+            maxHeap.add(s);
+        }        
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+            
+            if (y != x) {
+                maxHeap.add(y - x);
+            }
+        }       
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
