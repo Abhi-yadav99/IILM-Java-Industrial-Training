15e62c8f951bb258c5748538f4485fb01a6c13c2
abhishek kumar yadav <ay997326@gmail.com>
2026-04-03 17:19:12 +0530

    Time: 10 ms (75.69%), Space: 56.1 MB (6.81%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPossible(int[] target) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long sum = 0;
+
+        for (int x : target) {
+            pq.offer(x);
+            sum += x;
+        }
+        while (true) {
+            int max = pq.poll();
+            long rest = sum - max;
+            if (max == 1 || rest == 1) return true;
+            if (rest == 0 || max < rest) return false;
+            int prev = (int)(max % rest);
+            if (prev == 0) return false;
+            pq.offer(prev);
+            sum = rest + prev;
+        }
+    }
+}
